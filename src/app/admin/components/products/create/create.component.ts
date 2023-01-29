3d569b7b18853f87eb3976e48971846af19ed4cd
ibrahimgirisken <ibrahimgirisken@hotmail.com>
2023-01-29 11:33:01 +0300

    29.01.2023

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,4 +1,8 @@
-import { AlertifyService, MessageType, Position } from './../../../../services/admin/alertify.service';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from './../../../../services/admin/alertify.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from './../../../../base/base.component';
 import { Create_Product } from './../../../../contracts/create_product';
@@ -8,29 +12,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent extends BaseComponent implements OnInit {
-  constructor(spiner:NgxSpinnerService,private productService: ProductService,private alertify:AlertifyService){
+  constructor(
+    spiner: NgxSpinnerService,
+    private productService: ProductService,
+    private alertify: AlertifyService
+  ) {
     super(spiner);
   }
-  ngOnInit(): void {
-
-  }
-  create(name:HTMLInputElement,stock:HTMLInputElement,price:HTMLInputElement){
+  ngOnInit(): void {}
+  create(
+    name: HTMLInputElement,
+    stock: HTMLInputElement,
+    price: HTMLInputElement
+  ) {
     this.showSpinner(SpinnerType.BallRotate);
-    const create_product:Create_Product=new Create_Product();
-    create_product.name=name.value;
-    create_product.stock=parseInt(stock.value);
-    create_product.price=parseFloat(price.value);
+    const create_product: Create_Product = new Create_Product();
+    create_product.name = name.value;
+    create_product.stock = parseInt(stock.value);
+    create_product.price = parseFloat(price.value);
 
-    this.productService.create(create_product,()=>{
-      this.hideSpinner(SpinnerType.BallClipRotateMultiple);
-      this.alertify.message("Ürün başarı ile eklenmiştir !",{
-        dismissOthers:true,
-        messageType:MessageType.Success,
-        position:Position.TopRight
-      })});
+    this.productService.create(
+      create_product,
+      () => {
+        this.hideSpinner(SpinnerType.BallRotate);
+        this.alertify.message('Ürün başarı ile eklenmiştir !', {
+          dismissOthers: true,
+          messageType: MessageType.Success,
+          position: Position.TopRight,
+        });
+      },
+      (errorMessage) => {
+        this.alertify.message(errorMessage, {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight,
+        });
+      }
+    );
   }
-
 }
